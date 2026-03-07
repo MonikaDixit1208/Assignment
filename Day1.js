4914d0f3720279b047ae592b311be279260fe660
MonikaDixit1208 <monika19870812@gmail.com>
2026-03-07 14:25:22 +0530

    Add files via upload

---
+++ Day1.js
@@ -0,0 +1,15 @@
+let firstName='Tina'
+let companyName="Abc"
+let mobNum=9818101641
+let isAutomation=false
+let hasPlaywright
+console.log("First Name:"+firstName)
+console.log("type of First Name:"+typeof firstName)
+console.log("Company Name:"+companyName)
+console.log("type of Company Name:"+typeof companyName)
+console.log("mobile Number:"+mobNum)
+console.log("type of mobile Number:"+typeof mobNum)
+console.log("Automation:"+isAutomation)
+console.log("type of Automation:"+typeof isAutomation)
+console.log("has Playwright:"+hasPlaywright)
+console.log("type of has Playwright:"+typeof hasPlaywright)
---
+++ Day2.js
@@ -0,0 +1,9 @@
+let count=20
+console.log('Printing all odd numbers from 1 to 20')
+for(let i=1;i<=count;i++)
+{
+        if(i%2!=0)
+        {
+            console.log(`Odd number: ${i}`)
+        }
+}
